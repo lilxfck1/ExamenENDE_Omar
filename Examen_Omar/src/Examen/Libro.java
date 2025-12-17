287a210d082d4758bf3811c5b4db096f176ec6af
omahmoujdi <omahmoujdi1@educacion.navarra.es>
2025-12-17 10:38:05 +0100

    Métodos añadidos

--- Examen_Omar/src/Examen/Libro.java
+++ Examen_Omar/src/Examen/Libro.java
@@ -6,4 +6,48 @@ public class Libro {
     private String autor;
     private String isbn;
     private boolean prestado;
+    
+    public Libro(String titulo, String autor, String isbn, boolean prestado) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.prestado = prestado;
+    }
+    
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public boolean isPrestado() {
+        return prestado;
+    }
+
+    public void setPrestado(boolean prestado) {
+        this.prestado = prestado;
+    }
+
+    public String toString() {
+        return "Libro [titulo=" + titulo + ", autor=" + autor +
+               ", isbn=" + isbn + ", prestado=" + prestado + "]";
+    }
 }
